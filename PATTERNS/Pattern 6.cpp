d67611e324c7bc16a293adbe5fa88497885983ac
Ramu_64 <ramukotti90@gmail.com>
2026-05-13 09:29:31 +0530

    added pattern6 problem

---
+++ PATTERNS/Pattern 6.cpp
@@ -0,0 +1,17 @@
+//problem link: https://www.geeksforgeeks.org/problems/triangle-number-1661489840/1
+//timeComplexity: O(n^2)
+//spaceComplexity: O(1)
+
+class Solution {
+  public:
+
+    void printTriangle(int n) {
+        // code here
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n-i+1;j++){
+                cout<<j<<" ";
+            }
+            cout<<endl;
+        }
+    }
+};
