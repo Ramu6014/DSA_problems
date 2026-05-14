d6590ce97668954b152add7c509bc4d3dbb5c70f
Ramu_64 <ramukotti90@gmail.com>
2026-05-14 10:47:32 +0530

    added countDigits problem

---
+++ PATTERNS/Count Digits in a Number.java
@@ -0,0 +1,15 @@
+//problem link: https://www.geeksforgeeks.org/problems/count-total-digits-in-a-number/1
+//timeComplexity: O(log(K))
+//spaceComplexity: O(1)
+
+class Solution {
+    public static int countDigits(int n) {
+        // Code here
+        int cnt=0;
+        while(n>0){
+            cnt++;
+            n/=10;
+        }
+        return cnt;
+    }
+}
