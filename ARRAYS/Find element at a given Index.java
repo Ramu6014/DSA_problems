4b48976e9768aa74af934dfb489a02e10afb317c
Ramu_64 <ramukotti90@gmail.com>
2026-05-18 14:25:10 +0530

    added elementAtIndx problem

---
+++ ARRAYS/Find element at a given Index.java
@@ -0,0 +1,10 @@
+//problem link: https://www.geeksforgeeks.org/problems/c-array-print-an-element-set-25933/1?page=1&category=Arrays&difficulty=Basic&sortBy=submissions
+//timeComplexity: O(1)
+//spaceComplexity: O(1)
+
+class Solution {
+    public int findElementAtIndex(int i, int[] arr) {
+        // code here
+        return arr[i];
+    }
+}
