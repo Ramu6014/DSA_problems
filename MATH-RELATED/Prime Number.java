47a81d3932927ee53ecdf58f4f28945227f0f476
Ramu_64 <ramukotti90@gmail.com>
2026-05-23 23:55:03 +0530

    added primeNum problem

---
+++ MATH-RELATED/Prime Number.java
@@ -0,0 +1,18 @@
+//problem link: https://www.geeksforgeeks.org/problems/prime-number2314/1
+//timeComplexity: o(sqrt(n))
+//spaceComplexity: O(1)
+
+class Solution {
+    static boolean isPrime(int n) {
+        // code here
+        if(n==1){
+            return false;
+        }
+        for(int i=2;i*i<=n;i++){
+            if(n%i==0){
+                return false;
+            }
+        }
+        return true;
+    }
+}
