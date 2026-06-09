dc4aed8f7f1776e8be482fc652f900ec534fe6d5
Ramu_64 <ramukotti90@gmail.com>
2026-06-09 20:01:29 +0530

    added maxiTotalSubarrayValue problem

---
+++ ARRAYS/3689. Maximum Total Subarray Value I.java
@@ -0,0 +1,18 @@
+//problem link: https://leetcode.com/problems/maximum-total-subarray-value-i/?envType=daily-question&envId=2026-06-09
+//timeComplexity: o(n)
+//spaceComplexity: o(1)
+
+class Solution {
+    public long maxTotalValue(int[] nums, int k) {
+        int n=nums.length;
+        int maxi=Integer.MIN_VALUE;
+        int mini=Integer.MAX_VALUE;
+        for(int i=0;i<n;i++){
+            mini=Math.min(mini,nums[i]);
+            maxi=Math.max(maxi,nums[i]);
+        }
+        long val=maxi-mini;
+        long sum=val*k;
+        return sum;
+    }
+}
