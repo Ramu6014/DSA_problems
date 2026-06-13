6fe2de492113b30a2037f18004a2ac73a19fbfc1
Ramu_64 <ramukotti90@gmail.com>
2026-06-13 21:12:24 +0530

    added weightedWordMapping problem

---
+++ STRINGS/3838. Weighted Word Mapping.java
@@ -0,0 +1,20 @@
+//problem link: https://leetcode.com/problems/weighted-word-mapping/?envType=daily-question&envId=2026-06-13
+//timeComplexity: O(n*m)
+//spaceComplexity: O(1)
+
+class Solution {
+    public String mapWordWeights(String[] words, int[] weights) {
+        StringBuilder sb=new StringBuilder();
+        for(int i=0;i<words.length;i++){
+            String s=words[i];
+            int sum=0;
+            for(int j=0;j<s.length();j++){
+                sum+=weights[s.charAt(j)-'a'];
+            }
+            int val=sum%26;
+            char ch=(char)(122-val);
+            sb.append(ch);
+        }
+        return sb.toString();
+    }
+}
