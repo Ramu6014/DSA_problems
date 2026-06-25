09395ee6534f717fe8714a0a56f0ef9967f37930
Ramu_64 <ramukotti90@gmail.com>
2026-06-25 14:08:56 +0530

    added binarySearch problem

---
+++ BINARY-SEARCH/704. Binary Search.cpp
@@ -0,0 +1,24 @@
+//problem link: https://leetcode.com/problems/binary-search/
+//timeComplexity: O(log(n))
+//spaceComplexity: o(1)
+
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int n=nums.size();
+        int low=0,high=n-1;
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            if(target==nums[mid]){
+                return mid;
+            }
+            else if(target<nums[mid]){
+                high=mid-1;
+            }
+            else{
+                low=mid+1;
+            }
+        }
+        return -1;
+    }
+};
