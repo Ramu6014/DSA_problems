e604407ff343a33247fa16c2704a282b9c6d205a
Ramu_64 <ramukotti90@gmail.com>
2026-06-25 10:39:09 +0530

    modified the optimal code

--- ARRAYS/152. Maximum Product Subarray.cpp
+++ ARRAYS/152. Maximum Product Subarray.cpp
@@ -6,23 +6,20 @@ class Solution {
 public:
     int maxProduct(vector<int>& nums) {
         int n=nums.size();
-        int maxPr=INT_MIN;
-        int leftPr=1;
-        int rightPr=1;
+        int maxi=INT_MIN;
+        int prefix=1;
+        int suffix=1;
         for(int i=0;i<n;i++){
-            leftPr*=nums[i];
-            maxPr=max(maxPr,leftPr);
-            if(nums[i]==0){
-                leftPr=1;
+            if(prefix==0){
+                prefix=1;
             }
-        }
-        for(int i=n-1;i>=0;i--){
-            rightPr*=nums[i];
-            maxPr=max(maxPr,rightPr);
-            if(nums[i]==0){
-                rightPr=1;
+            if(suffix==0){
+                suffix=1;
             }
+            prefix*=nums[i];
+            suffix*=nums[n-i-1];
+            maxi=max(maxi,max(prefix,suffix));
         }
-        return maxPr;
+        return maxi;
     }
 };
