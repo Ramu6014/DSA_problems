1ac34d7ef37311057c5f586955a14192a216b337
Ramu_64 <ramukotti90@gmail.com>
2026-06-25 20:47:30 +0530

    added countSubarraysWithMajorityElements problem

---
+++ ARRAYS/3737. Count Subarrays With Majority Element I.cpp
@@ -0,0 +1,23 @@
+//problem link: https://leetcode.com/problems/count-subarrays-with-majority-element-i/?envType=daily-question&envId=2026-06-25
+//timeComplexity: O(n^2)
+//spaceComplexity: o(1)
+
+class Solution {
+public:
+    int countMajoritySubarrays(vector<int>& nums, int target){
+        int n=nums.size();
+        int ans=0;
+        for(int i=0;i<n;i++){
+            int cnt=0;
+            for(int j=i;j<n;j++){
+                if(nums[j]==target){
+                    cnt++;
+                }
+                if(cnt>(j-i+1)/2){
+                    ans++;
+                }
+            }
+        }
+        return ans;
+    }
+};
