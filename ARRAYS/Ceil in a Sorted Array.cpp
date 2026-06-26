57ac02e40ee4c441b101439792ba41a293b8c4f7
Ramu_64 <ramukotti90@gmail.com>
2026-06-26 22:10:02 +0530

    added ceilInSortedArray problem

---
+++ ARRAYS/Ceil in a Sorted Array.cpp
@@ -0,0 +1,24 @@
+//problem link: https://www.geeksforgeeks.org/problems/ceil-in-a-sorted-array/1
+//timeComplexity: o(log(n))
+//spaceComplexity: O(1)
+
+class Solution {
+  public:
+    int findCeil(vector<int>& arr, int x) {
+        // code here
+        int n=arr.size();
+        int indx=-1;
+        int low=0,high=n-1;
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            if(arr[mid]>=x){
+                indx=mid;
+                high=mid-1;
+            }
+            else{
+                low=mid+1;
+            }
+        }
+        return indx;
+    }
+};
