ddb405e177e884e0510543e55478d13669491e9b
Ramu_64 <ramukotti90@gmail.com>
2026-07-03 20:39:24 +0530

    added sqrt(x) problem

---
+++ BINARY-SEARCH/69. Sqrt(x).cpp
@@ -0,0 +1,23 @@
+//problem link: https://leetcode.com/problems/sqrtx/
+//timeComplexity: O(logn)
+//spaceComplexity: O(1)
+
+class Solution {
+public:
+    int mySqrt(int x) {
+        int low=1;
+        int high=x;
+        int ans=0;
+        while(low<=high){
+            long long mid=low+(high-low)/2;
+            if(mid*mid<=x){
+                ans=mid;
+                low=mid+1;
+            }
+            else{
+                high=mid-1;
+            }
+        }
+        return high;
+    }
+};
