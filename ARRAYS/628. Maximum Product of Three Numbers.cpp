a1bd76f852d75c4aad2a3e2192e0692d113a95fb
Ramu_64 <ramukotti90@gmail.com>
2026-07-27 11:57:57 +0530

    added maxProductThreeNum problem

---
+++ ARRAYS/628. Maximum Product of Three Numbers.cpp
@@ -0,0 +1,14 @@
+//problem link: https://leetcode.com/problems/maximum-product-of-three-numbers/?envType=daily-question&envId=2026-07-27
+//timeComplexity: o(nlogn)
+//spaceComplexity: o(1)
+
+class Solution {
+public:
+    int maximumProduct(vector<int>& nums) {
+        int n=nums.size();
+        sort(nums.begin(),nums.end());
+        int small=nums[0];
+        int secondSmall=nums[1];
+        return max(small*secondSmall*nums[n-1],nums[n-1]*nums[n-2]*nums[n-3]);
+    }
+};
