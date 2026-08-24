98fc6d3458981be32902ac24a5557d37cbf36195
Ramu_64 <ramukotti90@gmail.com>
2026-08-24 11:39:56 +0530

    added reverseLL problem

---
+++ LINKED-LISTS/206. Reverse Linked List.cpp
@@ -0,0 +1,28 @@
+//problem link: https://leetcode.com/problems/reverse-linked-list/
+//timeComplexity: O(n)
+//spaceComplexity: o(1)
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        ListNode*temp=head;
+        ListNode*last=NULL;
+        while(temp!=NULL){
+            ListNode*nextNode=temp->next;
+            temp->next=last;
+            last=temp;
+            temp=nextNode;
+        }
+        return last;
+    }
+};
