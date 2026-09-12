3d17b31b3066bf0216d6bc46ba7d4232486b52a9
Ramu_64 <ramukotti90@gmail.com>
2026-09-12 14:46:26 +0530

    added sortArr problem

---
+++ RECURSION/912. Sort an Array.cpp
@@ -0,0 +1,45 @@
+//problem link: https://leetcode.com/problems/sort-an-array/?envType=problem-list-v2&envId=merge-sort
+//timeComplexity: O(nlog(n))
+//spaceComplexity: O(1)
+
+class Solution {
+    void merge(int low,int mid,int high,vector<int>&nums){
+        int left=low,right=mid+1;
+        vector<int>temp;
+        while(left<=mid && right<=high){
+            if(nums[left]<=nums[right]){
+                temp.push_back(nums[left]);
+                left++;
+            }
+            else{
+                temp.push_back(nums[right]);
+                right++;
+            }
+        }
+        while(left<=mid){
+            temp.push_back(nums[left]);
+            left++;
+        }
+        while(right<=high){
+            temp.push_back(nums[right]);
+            right++;
+        }
+        for(int i=low;i<=high;i++){
+            nums[i]=temp[i-low];
+        }
+        return;
+    }
+    void mergeSort(int low,int high,vector<int>&nums){
+        if(low>=high)return;
+        int mid=low+(high-low)/2;
+        mergeSort(low,mid,nums);
+        mergeSort(mid+1,high,nums);
+        merge(low,mid,high,nums);
+    }
+public:
+    vector<int> sortArray(vector<int>& nums) {
+        int n=nums.size();
+        mergeSort(0,n-1,nums);
+        return nums;
+    }
+};
