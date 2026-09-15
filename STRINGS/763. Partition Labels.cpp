d06e194daa036de6eb0d761fe258e36bb60df748
Ramu_64 <ramukotti90@gmail.com>
2026-09-15 18:03:24 +0530

    added partitionLabels problem

---
+++ STRINGS/763. Partition Labels.cpp
@@ -0,0 +1,24 @@
+//problem link: https://leetcode.com/problems/partition-labels/
+//timeComplexity: o(n)
+//spaceComplexity: O(1)
+
+class Solution {
+public:
+    vector<int> partitionLabels(string s) {
+        vector<int> last(26, 0);
+        for (int i = 0; i < s.size(); i++) {
+            last[s[i] - 'a'] = i;
+        }
+        vector<int> ans;
+        int start = 0;
+        int end = 0;
+        for (int i = 0; i < s.size(); i++) {
+            end = max(end, last[s[i] - 'a']);
+            if (i == end) {
+                ans.push_back(end - start + 1);
+                start = i + 1;
+            }
+        }
+        return ans;
+    }
+};
